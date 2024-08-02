3cf8c6b39aec57c20cba806ebeee328e1b2621f1
Emiliano Sasal <94084079+emisasal@users.noreply.github.com>
2024-08-02 17:05:21 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import Markdown from "markdown-to-jsx"
+
+import mockNoticiaInterna from "./mockNoticiaInterna.json"
+
+console.log("mockNoticiaInterna", mockNoticiaInterna.Content.Text)
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1>Pruebas con MarkDown</h1>
       </header>
+      <main className="AppMain">
+        <hr />
+        <h2>- Markdown-to-JSX -</h2>
+
+        <div className="markdownReact">
+          <Markdown options={{ wrapper: "article" }}>
+            {mockNoticiaInterna.Content.Text}
+          </Markdown>
+        </div>
+        <hr />
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
